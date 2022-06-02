c21e29a47b8311e0500a3753024b440b2937bc38
Truong Nguyen <truongit001fe@gmail.com>
2022-06-02 10:43:02 +0700

    fix lay out

--- client/src/components/shop/HeaderShop.js
+++ client/src/components/shop/HeaderShop.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function HeaderShop() {
   return (
-    <div className='container-fluid' style={{marginTop:'60px'}}>
+    <div className='' style={{marginTop:'50px'}}>
         <img style={{width:'100%', maxHeight:'430px'}} src='https://cdn.shopify.com/s/files/1/1825/4753/files/slideshow.jpg?v=1637326488' alt='' />
     </div>
   )
